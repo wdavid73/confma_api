5931ee3cba356f3ec6c40fc1fd7ce8536a223d54
Wilson Padilla <wpadilla4@cuc.edu.co>
2020-10-09 12:10:44 -0500

    agregando endpoints al modulo de uniformes / sport

--- confma/Uniforms/Sports/Domain/urls.py
+++ confma/Uniforms/Sports/Domain/urls.py
@@ -6,7 +6,7 @@ from ..Application.GetByType import getMale, getFemale
 
 urlpatterns = [
     path('', GetAndPost.as_view(), name="uniforms_sports"),
-    path('male/', GetAndPost.as_view(), name="uniforms_sports_male"),
-    path('female/', GetAndPost.as_view(), name="uniforms_sports_female"),
+    path('male/', getMale, name="uniforms_sports_male"),
+    path('female/', getFemale, name="uniforms_sports_female"),
     path('<id>/', PutAndDelete.as_view(), name="uniform_sports_detail")
 ]
